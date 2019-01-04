98f781d982ab3909cf66e1573aa7f0f720cb3594
Kahr Christian (ITKG/ENG13.10) <christian.kahr2@de.bosch.com>
2019-01-04 09:51:19 +0100

    try

---
+++ compressLibs.ps1
@@ -0,0 +1,13 @@
+$currentLoc = $PWD;
+
+Set-Location -Path ($PSScriptRoot);
+
+if (Test-Path ./artLibs){ Remove-Item -Path ./artLibs };
+if (Test-Path ./artLibs.zip) {Remove-Item -Path ./artLibs.zip};
+
+New-Item -Path ./artLibs -ItemType Directory;
+Move-Item -Path .\bin\*.dll -Destination ./artLibs/
+
+Compress-Archive -Path ./artLibs -DestinationPath ./artLibs.zip
+
+Set-Location -Path $currentLoc;
